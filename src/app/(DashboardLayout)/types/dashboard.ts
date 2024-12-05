1325a4b4e4c2fdaf0f9526869cc9ffe7a1b00ea4
vanaf25 <vanayfefilov777@gmail.com>
2024-12-05 18:02:25 +0100

    addded system99

---
+++ src/app/(DashboardLayout)/types/dashboard.ts
@@ -0,0 +1,13 @@
+export interface ProductType {
+    cname: string;
+    email: string;
+    pname: string;
+    pimg: string;
+    rating: number;
+    stock: boolean;
+    photo: string;
+    id: number | string;
+    created: Date;
+    description: string;
+  }
+  
