5fb212fdff7b14950e7aa407e1384838ab7189c2
vanaf25 <vanayfefilov777@gmail.com>
2024-12-07 18:54:54 +0100

    addded system99

--- src/app/components/global/blueBlock/BlueBlock.tsx
+++ src/app/components/global/blueBlock/BlueBlock.tsx
@@ -3,14 +3,16 @@ import React from "react";
 
 interface BlueBlockProps {
   fullWidth?: boolean;
+  text?:string,
 }
 
-const BlueBlock: React.FC<BlueBlockProps> = ({ fullWidth }) => {
+const BlueBlock: React.FC<BlueBlockProps> = ({ fullWidth,text }) => {
   return (
       <TextField
           fullWidth={fullWidth}
           size="small"
           id="standard-basic"
+          defaultValue={text}
           label="Standard"
           variant="standard"
       />
