087aee372f6b29fa392cb9289adb0c6ce0fa2fde
vanaf25 <vanayfefilov777@gmail.com>
2025-01-04 22:22:11 +0100

    fixes url calculators

--- src/app/consts/calculators.ts
+++ src/app/consts/calculators.ts
@@ -1,6 +1,6 @@
 export  const CALCULATORS = [
-    "Gutters", "Walls", "LatticePanel", "HotTubPad", "HomeWrap", "VinylSiding", "Corners",
-    "SheetSiding", "Soffit", "PergolaShade", "FreezeBoard", "PlankSiding", "Fascia",
-    "StuccoWallCovering", "BrickWallCovering", "PorchCeiling", "StarFraming", "Fencing"
+    "Gutters", "Walls", "LaticalPanel", "HotTubPad", "HomeWrap", "VinylSiding", "Corners",
+    "SheetSiding", "Soffit", "PergolaShade", "FriezeBoard", "PlankSiding", "Fascia",
+    "StuccoWallCovering", "BrickWallCovering", "PorchCeiling", "StairFraming", "Fencing"
 ]
 export const typesOfProjects=["Windows","Deck","Siding"]
