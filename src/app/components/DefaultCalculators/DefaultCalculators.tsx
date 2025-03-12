7628ce2613ae603a72e21d775956670d345ca0e4
vanaf25 <vanayfefilov777@gmail.com>
2025-03-12 22:22:06 +0100

    calculators combined

--- src/app/components/DefaultCalculators/DefaultCalculators.tsx
+++ src/app/components/DefaultCalculators/DefaultCalculators.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react';
 import {Card, Grid, Typography} from "@mui/material";
 import CalculationValues from "@/app/components/CalculationValues/CalculationValues";
